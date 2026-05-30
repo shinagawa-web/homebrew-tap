e01b487900566065b89c42e97e13bfec4991f0f8
Kazutomo Deguchi <131741597+shinagawa-web@users.noreply.github.com>
2026-05-30 06:28:20 +0000

    Bump colref to v0.7.8

--- Formula/colref.rb
+++ Formula/colref.rb
@@ -1,26 +1,26 @@
 class Colref < Formula
   desc "Check whether a database column is still referenced in your codebase before you delete it"
   homepage "https://github.com/shinagawa-web/colref"
-  version "0.7.7"
+  version "0.7.8"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/shinagawa-web/colref/releases/download/v0.7.7/colref_0.7.7_darwin_arm64.tar.gz"
-      sha256 "1ff0befeef8698895194b8a67347ef390dc14fbe80ab86681dbe50e8329e59aa"
+      url "https://github.com/shinagawa-web/colref/releases/download/v0.7.8/colref_0.7.8_darwin_arm64.tar.gz"
+      sha256 "17bb710897922ddaa5f9f6a7947f934841ec236b2b1ee2c3b9c40088bf77547b"
     else
-      url "https://github.com/shinagawa-web/colref/releases/download/v0.7.7/colref_0.7.7_darwin_amd64.tar.gz"
-      sha256 "2d61ed7288db065809c6eb0b598a547f53f886220f71e19e10231c0964c70932"
+      url "https://github.com/shinagawa-web/colref/releases/download/v0.7.8/colref_0.7.8_darwin_amd64.tar.gz"
+      sha256 "16f55a4543bf66f8fb04801e08dc420e7150f538a269c257e4b5505eac7a0ba9"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/shinagawa-web/colref/releases/download/v0.7.7/colref_0.7.7_linux_arm64.tar.gz"
-      sha256 "65f56ad786e812afe8eaca3c4423bfc20aaf503f6b3af9050f73f8d5b5b605d0"
+      url "https://github.com/shinagawa-web/colref/releases/download/v0.7.8/colref_0.7.8_linux_arm64.tar.gz"
+      sha256 "765c4d24efe390dcfcda76193a7a732ff03ccf8c7bb548810096af47f0dc7f54"
     else
-      url "https://github.com/shinagawa-web/colref/releases/download/v0.7.7/colref_0.7.7_linux_amd64.tar.gz"
-      sha256 "7902a44eda2acd688ff969a2bb7bf9e76573fcabc091bca4033abe2610fb6ae4"
+      url "https://github.com/shinagawa-web/colref/releases/download/v0.7.8/colref_0.7.8_linux_amd64.tar.gz"
+      sha256 "056e9a653825d3312bf593a81ab5f56fcb1b021742cccd304656f5c00188a6cf"
     end
   end
 
