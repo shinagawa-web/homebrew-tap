0a57e306c10462658c2d186e9c39c10c873809f1
Kazutomo Deguchi <131741597+shinagawa-web@users.noreply.github.com>
2026-05-30 07:41:28 +0000

    Bump colref to v0.7.10

--- Formula/colref.rb
+++ Formula/colref.rb
@@ -1,26 +1,26 @@
 class Colref < Formula
   desc "Check whether a database column is still referenced in your codebase before you delete it"
   homepage "https://github.com/shinagawa-web/colref"
-  version "0.7.9"
+  version "0.7.10"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/shinagawa-web/colref/releases/download/v0.7.9/colref_0.7.9_darwin_arm64.tar.gz"
-      sha256 "548f009a2c72298f02279902fcef4ba9e8b2d7320356e1e7d20d9c272e732107"
+      url "https://github.com/shinagawa-web/colref/releases/download/v0.7.10/colref_0.7.10_darwin_arm64.tar.gz"
+      sha256 "cb94be88f995d48e2736e931a46aa44a07db26567f8332957abae440b56a5937"
     else
-      url "https://github.com/shinagawa-web/colref/releases/download/v0.7.9/colref_0.7.9_darwin_amd64.tar.gz"
-      sha256 "e8ebd99db6e48644c06f50093f81856c44d7c1310f897b22914a5ff95542dfd3"
+      url "https://github.com/shinagawa-web/colref/releases/download/v0.7.10/colref_0.7.10_darwin_amd64.tar.gz"
+      sha256 "181b580b6ee230d65c90d15d3460f7dc07bd5ae90c93ce0eff658e105e627dbe"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/shinagawa-web/colref/releases/download/v0.7.9/colref_0.7.9_linux_arm64.tar.gz"
-      sha256 "96633ad7c55adb1984913e927e59bcec4645cb0a1cf8844f8a63aaf6e2b756c9"
+      url "https://github.com/shinagawa-web/colref/releases/download/v0.7.10/colref_0.7.10_linux_arm64.tar.gz"
+      sha256 "b90bdb3604fa054e6e73166085dd153f51b122399d25ebeca6a8598bb667683f"
     else
-      url "https://github.com/shinagawa-web/colref/releases/download/v0.7.9/colref_0.7.9_linux_amd64.tar.gz"
-      sha256 "be915a307fa7a9d0e6d28df9e847e4bd166c1340530395ecd6221939f2b98b07"
+      url "https://github.com/shinagawa-web/colref/releases/download/v0.7.10/colref_0.7.10_linux_amd64.tar.gz"
+      sha256 "1ce4ef4a82ef9c67b7403ebaca95d65c53d9988509a5a0a985121f300acaaae1"
     end
   end
 
